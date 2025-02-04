963565e7a7e5c7efdc03dbce12c83d6d83f720c9
Your Name <you@example.com>
2025-02-04 14:32:58 +0000

    commit all changes

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -1,22 +1,3 @@
-
-
-provider "aws" {
-  region = var.region
-}
-
-terraform {
-  required_providers {
-    time = {
-      source = "hashicorp/time"
-      version = "0.12.1"
-    }
-  }
-}
-
-resource "time_sleep" "wait_for_ip" {
-  create_duration = "10s"  # Wait for 10 seconds
-}
-
 variable "region" {
   default = "us-east-1"
 }
@@ -49,8 +30,7 @@ resource "aws_instance" "vm" {
   }
 }
 
-output "vm_public_ip" {
-  value       = aws_instance.vm.public_ip
-  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
-  description = "Public IP address of the VM"
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "10s"  # Wait for 10 seconds
 }
---
+++ labs/lab101/providers.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    time = {
+      source = "hashicorp/time"
+      version = "0.12.1"
+    }
+  }
+}
