9106edf5e73c9d0a6088dece96207f6d2552f28b
Your Name <you@example.com>
2025-02-05 11:44:24 +0000

    data resource

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "vm" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  
+
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
@@ -46,15 +46,36 @@ variable "varcheck" {
 
 resource "null_resource" "check_public_ip" {
   provisioner "local-exec" {
+    environment = {
+      PUBLIC_IP = aws_instance.vm.public_ip
+    }
     command = <<EOT
       if [ -z "${aws_instance.vm.public_ip}" ]; then
         echo "ERROR: Public IP address was not assigned." >&2
         exit 1
         else
-        echo "ip is ${aws_instance.vm.public_ip}"
+        echo "IP is $PUBLIC_IP"
       fi
     EOT
   }
 
   depends_on = [aws_instance.vm]
 }
+
+
+
+# resource "null_resource" "with_triggers" {
+#   triggers = {
+#     build_number = "1.0.0"
+#   }
+#   provisioner "local-exec" {
+#     command = "echo 'Triggered on build number 1.0.0'"
+#   }
+# }
+
+# resource "null_resource" "after_vm" {
+#   depends_on = [aws_instance.vm]
+#   provisioner "local-exec" {
+#     command = "echo 'VM is created, now running commands.'"
+#   }
+# }
--- labs/lab102/part1/main.tf
+++ labs/lab102/part1/main.tf
@@ -1,19 +1,37 @@
-variable "varcheck" {
-  default = ""
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
 }
 
 
+# variable "varcheck" {
+#   default = ""
+# }
 
-resource "null_resource" "check_var" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if [ -z "${var.varcheck}" ]; then
-        echo "ERROR: var was not assigned." >&2
-        exit 1
-        else
-        echo "var not empty"
-      fi
-    EOT
-  }
+
+
+# resource "null_resource" "check_var" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       if [ -z "${var.varcheck}" ]; then
+#         echo "ERROR: var was not assigned." >&2
+#         exit 1
+#         else
+#         echo "var not empty"
+#       fi
+#     EOT
+#   }
+#   #depends_on = [aws_instance.vm]
+# }
+
+data "aws_instance" "machine_ip" {
+  instance_id = "i-09df7e0ed385f871b"
 }
 
+output "instance_ip" {
+  value = data.aws_instance.machine_ip.public_ip
+}
