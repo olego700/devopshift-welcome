443feb68e5d0a6ea6ee9d962001c603a66ca34c4
Your Name <you@example.com>
2025-02-05 14:12:18 +0000

    lab103

---
+++ labs/lab103/global.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+ region = var.region
+}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+# data "aws_instance" "terraform_ami" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["terraform-workshop-do-not-delete"]
+#   }
+# }
+
+variable "ami" {
+ default = "ami-0ecc0e0d5986a576d"
+ }
+variable "vm_name" {
+ default = "vm-[oleg]"
+}
+
+variable "admin_username" {
+ default = "admin-user"
+}
+
+variable "admin_password" {
+ default = "Password123!"
+}
+
+variable "vm_size" {
+ default = "t2.micro"
+}
